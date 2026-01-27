47df96a8c208cbf159fd41cf44e1786170e23abd
Ankit <ankitsingh110198@gmail.com>
2026-01-27 19:12:23 +0530

    front end backend url

--- src/routes/AdminRouter.ts
+++ src/routes/AdminRouter.ts
@@ -5,7 +5,14 @@ import { createAdmin } from "../controllers/adminController";
 const router = express.Router();
 
 /* ================= CREATE ADMIN ================= */
-router.post("/", createAdmin);
+// router.post("/", createAdmin);
+router.post("/", (req, res) => {
+  console.log("🔥 ADMIN ROUTE HIT");
+  console.log("🔥 HEADERS:", req.headers);
+  console.log("🔥 BODY:", req.body);
+
+  res.status(200).json({ ok: true });
+});
 
 /* ================= LIST ADMINS ================= */
 router.get("/", async (_req, res) => {
